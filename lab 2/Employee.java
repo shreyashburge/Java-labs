ebafab2187b4865417b036968f2bc25851653792
shreyashburge <134674249+shreyashburge@users.noreply.github.com>
2025-07-13 13:35:24 +0530

    Initial commit

---
+++ lab 2/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    protected int id;
+    protected int age= (int) 35.5;
+    protected String name;
+    protected boolean isPermanent;
+
+    public static void main(String[] args) {
+        System.out.println("Successfully started");
+    }
+}
---
+++ lab 2/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    // Private member variables
+    private String studentName;
+    private String collegeName;
+    private int studentID;
+
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Successful");
+    }
+}
