69946e963d27f834b3464cc9e0fb368df2afdc1d
shreyashburge <134674249+shreyashburge@users.noreply.github.com>
2025-07-14 14:32:02 +0530

    Initial commit - Java lab programs

---
+++ lab1/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    
+
+    String name = "Alice";
+    int age = 25;
+    int salary = 50000;
+
+    
+    public static void main(String[] args) {
+        System.out.println("Test Successful");
+    }
+}
+
